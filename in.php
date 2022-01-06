cb94f0cc3c9320bd23faee74fea0c15badec8fd0
denfromio <79191105+denfromio@users.noreply.github.com>
2022-01-06 13:04:03 +0200

    Update in.php

--- in.php
+++ in.php
@@ -5,18 +5,22 @@ while ( 1 ) {
   
   if ( is_file('/var/log/track/in.pending.log') ) {
     $o = null;
-    exec('cat /var/log/track/in.pending.log | clickhouse-client -q "INSERT INTO event FORMAT TSKV" 2>&1', $o, $r);
-    var_dump($r);
-    $o = implode("\n", $o);
+    exec('cat /var/log/track/in.pending.log | clickhouse-client -q "INSERT INTO event FORMAT TSKV" 2>&1', $o, $error);
     
-    $err = 'Unknown field found while parsing TSKV format: ';
-    if ( strpos($o, $err) ) {
-      $col = substr($o, strpos($o, $err) + strlen($err), strpos($o, ': (at row') - strpos($o, $err) - strlen($err));
-      echo 'we should add col: ' . $col . "\n";
-      exec('clickhouse-client -q "alter table event add column ' . $col . ' String"');
-      exec('cat /var/log/track/in.pending.log | clickhouse-client -q "INSERT INTO event FORMAT TSKV"');
+    if ( $error ) {
+      $o = implode("\n", $o);
+
+      $err = 'Unknown field found while parsing TSKV format: ';
+      if ( strpos($o, $err) ) {
+        $col = substr($o, strpos($o, $err) + strlen($err), strpos($o, ': (at row') - strpos($o, $err) - strlen($err));
+        echo 'we should add col: ' . $col . "\n";
+        exec('clickhouse-client -q "alter table event add column ' . $col . ' String"');
+        exec('cat /var/log/track/in.pending.log | clickhouse-client -q "INSERT INTO event FORMAT TSKV"');
+      }
+      else {
+        exec('cat /var/log/track/in.pending.log >> /var/log/track/in.error.log');
     }
-    
+
     unlink('/var/log/track/in.pending.log');
   }
 
