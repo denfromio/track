ca834913a2c38a9e1eccce7803a88f87ff92ddc8
denfromio <79191105+denfromio@users.noreply.github.com>
2022-01-06 12:52:17 +0200

    Update in.php

--- in.php
+++ in.php
@@ -4,7 +4,18 @@ while ( 1 ) {
   clearstatcache();
   
   if ( is_file('/var/log/track/in.pending.log') ) {
-    exec('cat /var/log/track/in.pending.log | clickhouse-client -q "INSERT INTO event FORMAT TSKV"');
+    $o = null;
+    exec('cat /var/log/track/in.pending.log | clickhouse-client -q "INSERT INTO event FORMAT TSKV" 2>&1', $o);
+    $o = implode("\n", $o);
+    
+    $err = 'Unknown field found while parsing TSKV format: ';
+    if ( strpos($o, $err) ) {
+      $col = substr($o, strpos($o, $err) + strlen($err));
+      $sol = substr($col, strpos($col, ':'));
+      
+      echo 'we should add col: ' . $col . "\n";
+    }
+    
     unlink('/var/log/track/in.pending.log');
   }
 
