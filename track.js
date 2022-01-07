f5a88fc1b00f6a32c61b858de463baf5cae65049
denfromio <79191105+denfromio@users.noreply.github.com>
2022-01-07 12:16:40 +0200

    Update track.js

--- track.js
+++ track.js
@@ -4,16 +4,13 @@ function remetric(id, metric, options, control) {
   options.metric = metric;
   options.container = id;
   
-  fd = new FormData();
-  Object.keys(options).forEach(key => fd.append(key, options[key]));
-  
   if ( control ) {
     control.classList.add('loading');
   }
   
   fetch('', {
     method: 'post',
-    body: fd
+    body: JSON.stringify(options)
   }).then(function(r) {
     return r.text();
   }).then(function(html) {
